94bb929753133c53b026bf7b73c252aeeb66aa99
braand4real <128923681+braand4real@users.noreply.github.com>
2023-03-26 07:27:35 +0200

    Update Brand.py

--- Brand.py
+++ Brand.py
@@ -1,9 +1,19 @@
 import discord
+from discord.ext import commands
 
-client = discord.Client()
+client = commands.Bot(command_prefix='@')
 
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
 
-client.run('MTA4ODkwMjI5MzQwOTI0NzQ1NQ.GL2ANn.G8OjAtvdWz25Z6xvZF8Kjl-dCSIaXlEcyshttI')
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+@client.event
+async def on_message(message):
+    if message.content == 'How are you?':
+        await message.channel.send('I am doing great, fuck you')
+
+client.run('TOKEN')
